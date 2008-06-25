9a24cedcbd87d803b0bf6d3e8615a05c3e8bb225
Mark Roddy <markroddy@gmail.com>
2008-06-25 04:05:34 +0000

    Adding intial package.  Python files with actual code to come.
    --HG--
    extra : convert_revision : svn%3A4e89a6ab-7450-0410-97d8-c518bcbbb747/trunk%402

---
+++ larvotto/__init__.py
@@ -0,0 +1,12 @@
+"""Larvotto AIM Bot"""
+
+
+__version__='0.1.0'
+__url__='http://code.google.com/p/larvotto'
+__license__='BSD'
+__author__='Mark Roddy'
+__contact__='markroddy _ at _ gmail'
+
+
+
+
---
+++ larvotto/commandline.py
@@ -0,0 +1,7 @@
+"""Utilities for working with Larvotto from the command line"""
+
+
+
+def main():
+    """Runs the Larvotto bot"""
+    pass
