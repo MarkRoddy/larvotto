e181b2e65e7bad1270d9447bdf49baaf5b14f538
Mark Roddy <markroddy@gmail.com>
2008-06-26 03:00:27 +0000

    Added class to be used for handling communication with instant messages
    --HG--
    extra : convert_revision : svn%3A4e89a6ab-7450-0410-97d8-c518bcbbb747/trunk%4012

--- larvotto/bot.py
+++ larvotto/bot.py
@@ -1,2 +1,21 @@
 """Implements the instant messager bot that handles communication"""
 
+from toc import TocTalk
+import larvotto.response
+
+
+class MarkovBot(TocTalk):
+	"""Handles all IM connection issues"""
+
+	_resp=None
+
+	def __init__(self,scnname,passwd,response):
+		"""
+		@param scnname: AIM Screen Name
+		@param passwd: AIM password
+		@param markov:
+		"""
+		assert isinstance(response,larvotto.response.BaseResponse)
+		TocTalk.__init__(self,scnname,passwd)
+		self._resp=response
+
