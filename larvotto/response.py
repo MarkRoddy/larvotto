832c879770e028566947a8301340ef81f96235f0
Mark Roddy <markroddy@gmail.com>
2008-06-26 03:10:46 +0000

    Added class Echo to the response module.  This mainly serves as an example of a response class, but can also be used for testing purposes.  Also added unit test for the class.
    --HG--
    extra : convert_revision : svn%3A4e89a6ab-7450-0410-97d8-c518bcbbb747/trunk%4014

--- larvotto/response.py
+++ larvotto/response.py
@@ -6,8 +6,14 @@ import larvotto.convsrc
 class BaseResponse(object):
 	"""Defines the interface from which all 'response' objects must inherit"""
 
-	def get(self,scnname,query):
+	def get(self,scnname,msg):
 		"""Get a response to an incoming instant message"""
 		raise NotImplementedError
 
 
+class Echo(BaseResponse):
+	"""Echos the incoming message back to the user"""
+
+	@staticmethod
+	def get(scnname,msg):
+		return msg
--- larvotto/tests/response.py
+++ larvotto/tests/response.py
@@ -4,11 +4,15 @@ import unittest
 import larvotto.response
 
 
+class EchoTest(unittest.TestCase):
+	"""Unit test for the L{larvotto.response.Echo} class"""
 
-class BaseResponseTest(object):
-    """Set of tests that all response objects must pass"""
+	def setUp(self):
+		self.resp=larvotto.response.Echo()
 
-
+	def testGetEcho(self):
+		s='Some Message To Echo'
+		self.assertEquals(s,self.resp.get('user',s))
 
 
 if __name__=='__main__':
