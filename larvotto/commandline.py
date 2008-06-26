2166fd58ee408f98f9a75d503878769b93264884
Mark Roddy <markroddy@gmail.com>
2008-06-26 04:09:14 +0000

    Starting work on the commandline module which implements command line functionality. also updated test script, though more needs to be implemented before any tests can be written
    --HG--
    extra : convert_revision : svn%3A4e89a6ab-7450-0410-97d8-c518bcbbb747/trunk%4018

--- larvotto/commandline.py
+++ larvotto/commandline.py
@@ -1,7 +1,18 @@
 """Utilities for working with Larvotto from the command line"""
 
+import larvotto
 
+from optparse import OptionParser
+import sys
 
 def main():
-    """Runs the Larvotto bot"""
-    pass
+	"""Runs the Larvotto bot"""
+	p=OptionParser(version='%progv'+larvotto.__version__)
+	p.add_option('-p','--password',dest='passwd',help='Specify Password at Commandline')
+	(opts,args)=p.parse_args()
+	if 1!=len(args):
+		print p.usage
+		sys.exit(1)
+
+	
+	
---
+++ larvotto/tests/commandline.py
@@ -0,0 +1,12 @@
+"""Unit tests for the L{larvotto.commandline} module"""
+
+import unittest
+import larvotto.commandline
+
+
+
+
+
+
+if __name__=='__main__':
+	unittest.main()
