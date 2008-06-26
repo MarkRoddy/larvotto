3adf9dd2022c43806bd05bba2b7253a4e898d87b
Mark Roddy <markroddy@gmail.com>
2008-06-26 01:49:36 +0000

    Renamed markov module to response as responses to incoming instant messages may not necessarily be generated using markov techniques.
    --HG--
    rename : larvotto/markov.py => larvotto/response.py
    extra : convert_revision : svn%3A4e89a6ab-7450-0410-97d8-c518bcbbb747/trunk%409

--- larvotto/markov.py
+++
@@ -1,7 +0,0 @@
-"""Provides routines for generating randomized text based on a data source"""
-
-
-import larvotto.convsrc
-
-
-
---
+++ larvotto/response.py
@@ -0,0 +1,7 @@
+"""Provides routines for generating different responses to incoming messages"""
+
+
+import larvotto.convsrc
+
+
+
