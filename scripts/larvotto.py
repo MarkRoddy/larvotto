288dad3e8e866c4f1e52954f909f25a8de114f5d
Mark Roddy <markroddy@gmail.com>
2008-06-26 04:01:57 +0000

    Updated script files so that the removed the cwd from python path if there is a python file in the cwd with the same name as the main package.  Much love to epydoc who came up with this solution.
    --HG--
    extra : convert_revision : svn%3A4e89a6ab-7450-0410-97d8-c518bcbbb747/trunk%4017

--- scripts/larvotto.py
+++ scripts/larvotto.py
@@ -1,4 +1,10 @@
 
+import sys,os
+if os.path.exists(os.path.join(sys.path[0], 'larvotto.py')):
+	 del sys.path[0]
+
+import larvotto
+print larvotto.__file__
 from larvotto.commandline import main
 
 main()
