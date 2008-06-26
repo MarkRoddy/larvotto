3df7b85552aff9c3789e6a6dbfaf47f04f592aae
Mark Roddy <markroddy@gmail.com>
2008-06-26 02:58:59 +0000

    Added base class for response objects, and created test file for the module.
    --HG--
    extra : convert_revision : svn%3A4e89a6ab-7450-0410-97d8-c518bcbbb747/trunk%4011

--- larvotto/response.py
+++ larvotto/response.py
@@ -3,5 +3,11 @@
 
 import larvotto.convsrc
 
+class BaseResponse(object):
+	"""Defines the interface from which all 'response' objects must inherit"""
+
+	def get(self,scnname,query):
+		"""Get a response to an incoming instant message"""
+		raise NotImplementedError
 
 
