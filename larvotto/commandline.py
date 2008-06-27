493d71f9066e6b83ab6a9d692d2773ef3dd5b21b
Mark Roddy <markroddy@gmail.com>
2008-06-27 03:20:10 +0000

    Created a first version of the main() function used by the command line which only uses the echo response class.
    --HG--
    extra : convert_revision : svn%3A4e89a6ab-7450-0410-97d8-c518bcbbb747/trunk%4022

--- larvotto/commandline.py
+++ larvotto/commandline.py
@@ -1,18 +1,23 @@
 """Utilities for working with Larvotto from the command line"""
 
 import larvotto
-
+import larvotto.bot
+import larvotto.response
 from optparse import OptionParser
+from getpass import getpass
 import sys
 
 def main():
 	"""Runs the Larvotto bot"""
-	p=OptionParser(version='%progv'+larvotto.__version__)
-	p.add_option('-p','--password',dest='passwd',help='Specify Password at Commandline')
+	p=OptionParser(usage='%prog [options] screenname',version='%progv'+larvotto.__version__)
+	p.add_option('-p','--password',dest='passwd',default='',help='Specify Password at Commandline')
 	(opts,args)=p.parse_args()
 	if 1!=len(args):
-		print p.usage
+		p.print_help()
 		sys.exit(1)
-
-	
-	
+	else:
+		if opts.passwd:
+			passwd=opts.passwd
+		else:
+			passwd=getpass('Password: ')
+		larvotto.bot.Start(args[0],passwd,larvotto.response.Echo())
