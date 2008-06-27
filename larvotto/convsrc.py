5daa55ab197b0456b4061af8baaa075a9f70c14a
Mark Roddy <markroddy@gmail.com>
2008-06-27 02:16:17 +0000

    Completed implementation of the PidginLogs() function which parses log files from Pidgin instant messager.
    --HG--
    extra : convert_revision : svn%3A4e89a6ab-7450-0410-97d8-c518bcbbb747/trunk%4020

--- larvotto/convsrc.py
+++ larvotto/convsrc.py
@@ -4,12 +4,24 @@ All functions return an interable whose items are tuples in the form of:
 (date/time,screenname,message)
 """
 
-import os
+import os,re
+from datetime import datetime
 
 
 def PidginLogs(LogDir):
 	"""Returns all conversations by parsing Pidgin log files"""
 	assert os.path.isdir(LogDir), 'Not a directory: '+str(LogDir)
-	
-
-    
+	messages=[]
+	recordre=re.compile(r'^\((\d\d\:\d\d\:\d\d\s+[A|P]M)\)\s+(.*?)\:\s+(.*)\s*$', re.I|re.L)
+	for d in [LogDir+os.sep+f for f in os.listdir(LogDir) if os.path.isdir(LogDir+os.sep+f)]:
+		for logf in os.listdir(d):
+			doy=logf.split('.')[0]
+			logf=d+os.sep+logf
+			for rec in open(logf).readlines()[1:]:
+				m=recordre.search(rec.strip()) 
+				if not m:
+					raise ValueError("malformed log record: '%s'"%rec.strip())
+				tod,scrnname,msg=m.groups()
+				dtime=datetime.strptime('%s %s'%(doy,tod), '%Y-%m-%d %I:%M:%S %p')
+				messages.append( (dtime,scrnname,msg,) )
+	return messages
