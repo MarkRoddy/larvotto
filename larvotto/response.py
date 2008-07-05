e30ce4b9a6f67e6529edca238436918e98cce2eb
Mark Roddy <markroddy@gmail.com>
2008-07-05 18:46:49 +0000

    Added repsonse class MarkovChain which generates responses using markov chain techinques based on a specified user and his previous AIM text.
    --HG--
    extra : convert_revision : svn%3A4e89a6ab-7450-0410-97d8-c518bcbbb747/trunk%4041

--- larvotto/response.py
+++ larvotto/response.py
@@ -1,7 +1,6 @@
 """Provides routines for generating different responses to incoming messages"""
 
-
-import larvotto.convsrc
+import random
 
 class BaseResponse(object):
 	"""Defines the interface from which all 'response' objects must inherit"""
@@ -17,3 +16,51 @@ class Echo(BaseResponse):
 	@staticmethod
 	def get(scnname,msg):
 		return msg
+
+class MarkovChain(BaseResponse):
+	"""
+	Generates a response to a user message using markov chain text generation
+	For this basic implementation, all previous messages sent by the user are
+	used to seed the markov chain.
+	"""
+
+	_screenname=None
+	_precision=None
+
+	def __init__(self, ScreenName, ConvoRecords, precision=4):
+		"""
+		ConvoRecords is a list of tuples in the form of (datetime,screenname,message)
+		"""
+		self._buildmap([r for r in ConvoRecords if r[1]==ScreenName], precision)
+		self._screenname=ScreenName
+		self._precision=precision
+
+	def _buildmap(self, records, precision):
+		wordmap={}
+		for r in records:
+			self._addSentenceToMap(wordmap, r[2].split(), precision)
+				
+		self.wordmap=wordmap
+
+	@staticmethod
+	def _addSentenceToMap(wordmap, words, precision):
+		if precision< len(words):
+			i=0
+			while (i+precision) < len(words):
+				wordmap.setdefault(tuple(words[i:i+precision]),[]).append(words[i+precision])
+				i+=1
+			wordmap.setdefault(tuple(words[-precision:]),[]).append(None)
+		else:
+			wordmap[tuple(words)]=[None]
+
+
+	def get(self,scnname,msg):
+		resp=list(random.choice(self.wordmap.keys()))
+		if self._precision!=len(resp):
+			return resp
+		else:
+			newword=random.choice(self.wordmap[tuple(resp[-self._precision:])])
+			while newword:
+				newword=random.choice(self.wordmap[tuple(resp[-self._precision:])])
+				resp.append(newword)
+		return ' '.join([w for w in resp if w])
--- larvotto/tests/response.py
+++ larvotto/tests/response.py
@@ -15,5 +15,27 @@ class EchoTest(unittest.TestCase):
 		self.assertEquals(s,self.resp.get('user',s))
 
 
+
+class MarkovTest(unittest.TestCase):
+	"""Unit test for the L{larvotto.response.MarkovChain} class"""
+	precision=2
+	convos=[
+		(None,'me', "hey, how's it going"),
+		(None,'you',"ah ok I guess"),
+		(None,'me', "did you see the show?"),
+		(None,'you',"no it was sold out"),
+		(None,'me','Yes!')]
+
+	def setUp(self):
+		self.resp=larvotto.response.MarkovChain('me',self.convos,self.precision)
+
+	def testGet(self):
+		resp=self.resp.get('you','hey there')
+		self.assert_(isinstance(resp,basestring))
+		self.assertNotEquals(0, len(resp))
+
+
+
+
 if __name__=='__main__':
 	unittest.main()
