d976cd79b452adc76cc2a726ab433a6bc703e486
Mark Roddy <markroddy@gmail.com>
2008-07-05 19:06:49 +0000

    Fixed bug in regular expression definition to allow a single digit hour to be handled.  As part of this fix, pulled the parsing of a single record into a seperate function so that it could be tested on its own.  Also added unit test to check that this bug does not reappear.
    --HG--
    extra : convert_revision : svn%3A4e89a6ab-7450-0410-97d8-c518bcbbb747/trunk%4043

--- larvotto/tests/convsrc.py
+++ larvotto/tests/convsrc.py
@@ -5,15 +5,19 @@ import larvotto.convsrc
 
 
 class TestPidginLogs(unittest.TestCase):
-    """Test case for the L{larvotto.convsrc.PidginLogs} function"""
+	"""Test case for the L{larvotto.convsrc.PidginLogs} function"""
 
 
-    def testNonDirectoryPath(self):
-        if __debug__:
-            self.assertRaises(AssertionError, larvotto.convsrc.PidginLogs, '')
+	def testNonDirectoryPath(self):
+		if __debug__:
+			self.assertRaises(AssertionError, larvotto.convsrc.PidginLogs, '')
 
 
+	def testSingleDigitHour(self):
+		record='(1:12:16 PM) MyScreenName: test'
+		d='2008-07-05'
+		larvotto.convsrc._ParsePidginRecord(record,d)
 
 
 if __name__=='__main__':
-    unittest.main()
+	unittest.main()
