d602a8eced5cfd3c75cc27e7f6bf35fa43409806
Mark Roddy <markroddy@gmail.com>
2009-10-04 21:22:40 +0000

    Removed control characters added by a windows based text editor.
    --HG--
    extra : convert_revision : svn%3A4e89a6ab-7450-0410-97d8-c518bcbbb747/trunk%4085

--- larvotto/tests/convsrc.py
+++ larvotto/tests/convsrc.py
@@ -7,28 +7,23 @@ import larvotto.convsrc
 class TestPidginLogs(unittest.TestCase):
 	"""Test case for the L{larvotto.convsrc.PidginLogs} function"""
 
-
 	def testNonDirectoryPath(self):
 		if __debug__:
 			self.assertRaises(AssertionError, larvotto.convsrc.PidginLogs, '')
-
 
 	def testSingleDigitHour(self):
 		record='(1:12:16 PM) MyScreenName: test'
 		d='2008-07-05'
 		larvotto.convsrc._ParsePidginRecord(record,d)
 
-
 	def testNoAMPM(self):
 		record='(22:20:45) MyScreeName: those are some tasty burgers'
 		d='2008-07-05'
 		larvotto.convsrc._ParsePidginRecord(record,d)
 
-
 	def testSignOffSystemMessage(self):
 		msg='(1:42:07 PM) AIMsweringMachin has signed off.'
 		self.assert_(larvotto.convsrc._IsSystemMessage(msg))
-
 
 	def testLoggedOffSystemMessage(self):
 		msg='(04:34:28) Ahs123 logged out.'
@@ -41,7 +36,6 @@ class TestPidginLogs(unittest.TestCase):
 	def testLoggedInSystemMessage(self):
 		msg='(00:51:44) Ahs123 logged in.'
 		self.assert_(larvotto.convsrc._IsSystemMessage(msg))
-
 
 if __name__=='__main__':
 	unittest.main()
